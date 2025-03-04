0e7b76bcd81f8b4ec7f05f4057b41ea5c59d96df
Pedro Cedeño <pcedenoj29@gmai.com>
2025-03-04 15:11:31 -0500

    Mantenimiento inicial de la entidad device

--- src/app/modules/crud/device/device.component.ts
+++ src/app/modules/crud/device/device.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DeviceI } from '../../../interfaces/device.interface';
+import { Modal } from 'bootstrap';
+
 
 @Component({
   selector: 'app-device',
@@ -7,6 +9,9 @@ import { DeviceI } from '../../../interfaces/device.interface';
   styles: ``
 })
 export class DeviceComponent {
+
+  @ViewChild('deviceModal') deviceModal!: Modal
+
 
   deviceList: DeviceI[] = [
     {
@@ -24,5 +29,23 @@ export class DeviceComponent {
       name: 'Redmi Note 14 pro',
       quantity: 15
     }
-]
+  ]
+
+  //Este metodo va a recibir los datos actualizados del producto que se introdujeron en el modal.
+  //find() es un método de los arrays en JavaScript/TypeScript que busca el primer elemento que cumpla con una condición.
+  updateTable(deviceUpdate: DeviceI){
+    let findDevice = this.deviceList.find(device => device.id === deviceUpdate.id);
+
+    if(findDevice){
+      let devicePosition = this.deviceList.indexOf(findDevice);
+
+      //Aqui se utiliza el slice para eliminar 
+      this.deviceList.splice(devicePosition, 1);
+      this.deviceList.push(deviceUpdate);
+      this.deviceList.sort( (device1, device2) => device1.id - device2.id);
+    }
+  }
 }
+
+//? indexOf(findDevice) busca la posición de findDevice dentro del array deviceList y la guarda en devicePosition. 
+// ? this.deviceList.splice(devicePosition, 1) ---> El número 1 indica que solo queremos eliminar un único elemento, que es el dispositivo encontrado en devicePosition.
--- src/app/modules/shared/crud-button/crud-button.component.ts
+++ src/app/modules/shared/crud-button/crud-button.component.ts
@@ -1,14 +1,36 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, AfterViewInit, OnInit } from '@angular/core';
+import { DeviceI } from '../../../interfaces/device.interface';
 
 @Component({
   selector: 'app-crud-button',
   templateUrl: './crud-button.component.html',
   styles: ``
 })
-export class CrudButtonComponent {
+export class CrudButtonComponent implements AfterViewInit, OnInit {
 
+  //aqui se inicializan los valores por defecto 
+  //Cuando se rendericen todos los componentes de la pagina (cuando se cargue el boton), es decir, carguen complemente, 
+  // toda la informacion de la fila se va a cargar sobre el boton y el boton va a coger esa informacion y la va almacenar en la
+  //variable deviceUpdate para que si se hace algun cambio la informacion se mantenga a salva y solo si se le da a guardar esa informacion se actualizara.
+
+  name:string = '';
+  quantity: number = 0;
+
+  ngOnInit(): void {
+    this.name = this.device.name;
+    this.quantity = this.device.quantity;
+    
+  }
+
+  //metodo que se ejecuta justo despues de cargar la vista
+  ngAfterViewInit(): void {
+    //this.deviceUpdate = this.device;
+  }
+
+  
+  
   // ? EVENTOS DE ENTRADA 
-  // ? Atibutos parametros de entrada de personalización del componente
+  // ? Atributos o  parametros de entrada de personalización del componente
   //Color del boton
   @Input()
   styleButton: string = 'secondary' //valor por default: secondary(gris)
@@ -31,12 +53,37 @@ export class CrudButtonComponent {
   @Input()
   disabled: boolean = false;
 
+  //informacion de entrada
+  //Se crea una variable de @Input para que cuando se rendericen todos los componentes en el componente padre y se recorra la lista de dispositivos
+  //del componente padre estos dispositivos se pasen a esta variable device del componente hijo y asi se vayan almacenando los datos de cada dispositivos
+  //para posteriormente poder modificarlos.
+  
+  @Input()
+  device: DeviceI = {
+    id: 0,
+    name: '',
+    quantity: 0
+  }
+
 
   //? EVENTOS DE SALIDA
   @Output() clickEvent =  new EventEmitter(); 
 
+  @Output() clickSave =  new EventEmitter();
+
   clickEventUpdate(){
-    this.clickEvent.emit()
+    this.clickEvent.emit();
+  }
+
+  //Cuando se ejecute este metodo clickEventSave() en el componente hijo, este mismo va a emitir un evento clickSave para notificar al componente padre.
+  //Cuando se ejecute este metodo this.clickSave.emit(deviceUpdate); envía los datos actualizados del dispositivo al componente padre.
+  clickEventSave(){
+    let deviceUpdate: DeviceI = {
+      id: this.device.id,
+      name: this.name,
+      quantity: this.quantity
+    }
+    this.clickSave.emit(deviceUpdate);
   }
 
 }
