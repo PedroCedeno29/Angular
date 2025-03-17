fa198874fecc66613074614cfefef6b88f5e3a33
Pedro Cedeño <pcedenoj29@gmai.com>
2025-03-16 21:19:55 -0500

    Interaccion entre tablas de carrito version 1

--- src/app/modules/shopcart/product-cart/product-cart.component.ts
+++ src/app/modules/shopcart/product-cart/product-cart.component.ts
@@ -7,6 +7,8 @@ import { ProductDetailI } from '../../../interfaces/productdetail.interface';
   templateUrl: './product-cart.component.html',
   styles: ``
 })
+
+//? Product cart es el componente principal y es quien recibe los registros de productos en stock y los asigna mediante el ViewChild sobre su otro componente hijo TableProductSelected
 export class ProductCartComponent {
   //el signo de exclamación indica que ese elemento si existe que no va a ser nulo.
   //@ViewChild(TableProductSelectedComponent) indica que queremos acceder a una instancia del componente TableProductSelectedComponent 
@@ -14,6 +16,45 @@ export class ProductCartComponent {
   @ViewChild(TableProductSelectedComponent) productSelected!:  TableProductSelectedComponent;
 
   aggProduct(product:ProductDetailI){
-    this.productSelected.listaProductSelected.push(product)
+    let productSelected = product.product;
+    let productCategorySelected = product.category;
+    let productPriceSelected = product.price;
+    let productIdSelected = product.id;
+
+    //aqui vamos a comprobar si la lista de productos tiene algun producto.S
+    if(this.productSelected.listaProductSelected.length > 0){
+      //Validar si existe o no el producto.
+      let productValidation = this.productSelected.listaProductSelected.find(productInCart => productInCart.id == productIdSelected);
+
+      if(productValidation){
+        this.productSelected.listaProductSelected.map(productInCart => {
+          if(productInCart.id == productValidation.id){
+            productInCart.stock++;
+          }
+        })
+      } else {
+        let newProduct: ProductDetailI = {
+          id: productIdSelected,
+          product: productSelected,
+          category: productCategorySelected,
+          price: productPriceSelected,
+          provider: '',
+          stock: 1
+        }
+        this.productSelected.listaProductSelected.push(newProduct)
+      }
+      //Este else es para cuando se va a agregar el primer producto al carrito de compras. Una vez que el carrito ya tiene 1 producto
+      //agregado procede con la logica del if.
+    }else{
+      let newProduct: ProductDetailI = {
+        id: productIdSelected,
+        product: productSelected,
+        category: productCategorySelected,
+        price: productPriceSelected,
+        provider: '',
+        stock: 1
+      }
+      this.productSelected.listaProductSelected.push(newProduct)
+    }
   }
 }
